ff73d903fbfc52c0a6088d3169be6d4365540bed
kruno89 <kkukuc031@gmail.com>
2019-02-26 16:40:16 +0100

    5.2.6. dobar_broj_1

---
+++ ConsoleApp1/5.2.6.dobar_broj_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._6.dobar_broj_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijeli broj:");
+            int a = int.Parse(Console.ReadLine());
+            if (a%5 == 0 || a%3 == 0)
+            {
+                Console.Write("Broj je dobar.");
+            }
+            else
+            {
+                Console.Write("Broj nije dobar!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
