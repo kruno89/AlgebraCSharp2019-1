158bc6e90565935530b2b98e78b773d1bb374fad
kruno89 <kkukuc031@gmail.com>
2019-02-26 16:11:40 +0100

    zbirka 5.2.3. i 5.2.4.

---
+++ ConsoleApp1/5.2.3.Velik Broj/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._3.Velik_Broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj: ");
+
+            int a = int.Parse(Console.ReadLine());
+            
+            if (a >= 100)
+            {
+                Console.WriteLine("Broj je velik.");
+
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE velik.");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/5.2.4.znamenkasti_broj/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._4.znamenkasti_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijeli broj: ");
+
+            int a = int.Parse(Console.ReadLine());
+
+            if (a < 0) a = -a;
+
+            if (a < 10)
+            {
+                Console.WriteLine("Uneseni broj je jednoznamenkast.");
+
+            }
+            else if (a < 100)
+            {
+                Console.WriteLine("Uneseni broj je dvoznamenkast.");
+
+            }
+            else if (a < 1000)
+            {
+                Console.WriteLine("Uneseni broj je troznamenkast.");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj je višeznamenkast.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
