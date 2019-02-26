0bcf4ae8ccae8ec1c158e3003f5ff2923fcc9b34
kruno89 <kkukuc031@gmail.com>
2019-02-26 18:36:47 +0100

    suma prvih 100

---
+++ ConsoleApp1/suma_prvih_100/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_prvih_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nSuma prvih 100");
+            int suma = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("{0} ", suma += i);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
