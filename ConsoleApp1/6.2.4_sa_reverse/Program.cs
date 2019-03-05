539aef6a5fd0a3795e4e5619c9a8f223ef002120
kruno89 <kkukuc031@gmail.com>
2019-03-05 18:17:22 +0100

    nizovi i liste, novi primjeri

---
+++ ConsoleApp1/6.2.4_sa_reverse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._4_sa_reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulaz_broj = Console.ReadLine();
+            int broj = int.Parse(ulaz_broj);
+            ulaz_broj.Reverse();
+            //new string(ulaz_broj.Reverse().ToArray()) //izbaciš ako hoćeš da vrati na staro
+            Console.WriteLine("Original: {0} Obrnuti: {1} ", broj, new string(ulaz_broj.Reverse().ToArray()));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/6.2.4_sa_reverse/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("6.2.4_sa_reverse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("6.2.4_sa_reverse")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("32efa1c5-30e4-41d1-ac1e-facf7310e030")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
