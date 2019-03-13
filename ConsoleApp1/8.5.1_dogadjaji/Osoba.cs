87623d07b5d6fc9293b3b209282b6e0d0e78a1df
kruno89 <kkukuc031@gmail.com>
2019-03-13 17:12:39 +0100

    dodan primjer 8.5.1 dogadjaji prirucnik

---
+++ ConsoleApp1/8.5.1_dogadjaji/Osoba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8._5._1_dogadjaji
+{
+    public  class Osoba
+    {
+        // Deklaracija događaja
+        public delegate void NaPromjenuImenaDelegat(object sender, EventArgs e);
+        public event NaPromjenuImenaDelegat NaPromjenuImena;
+
+        // Svojstva
+        private string ime;
+        public string Ime
+        {
+            get { return ime; }
+            set
+            {
+                ime = value;
+                if (NaPromjenuImena != null)
+                {
+                    NaPromjenuImena(this, new EventArgs());
+                }
+            }
+        }
+    }
+}
