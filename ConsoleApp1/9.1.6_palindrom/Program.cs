8d4a167a0a0df0158984fa0c38422a3fc60451ad
kruno89 <kkukuc031@gmail.com>
2019-03-14 12:58:35 +0100

    dodana zadaca 9.1.6 zbirka, palindrom

---
+++ ConsoleApp1/9.1.6_palindrom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1._6_palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite tekst: ");
+            string tekst = Console.ReadLine();
+
+            // Ovdje uklanjamo razmake
+            tekst = tekst.Replace(" ", "");
+
+            // String varijabla zdesna na lijevo
+            string obrnuto = "";
+            for (int i = tekst.Length - 1; i >= 0; i--)
+            {
+                obrnuto += tekst[i];
+            }
+
+            // palindrom
+            if (tekst == obrnuto)
+            {
+                Console.WriteLine("To je PALINDROM.");
+            }
+            else
+            {
+                Console.WriteLine("To NIJE PALINDROM.");
+            }
+            Console.ReadKey();
+          
+
+        }
+    }
+}
