e389c15c2704451dc0f10c4c78f5777ddd53909f
kruno89 <kkukuc031@gmail.com>
2019-03-15 17:30:57 +0100

    zadatak za ponavljanje

---
+++ ConsoleApp1/14.1.1_ponavljanje_zbirka/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._1._1_ponavljanje_zbirka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pogledati kod kuće
+            // instaliraj class diagram
+            // a prvo nabavi class designer
+        }
+    }
+}
