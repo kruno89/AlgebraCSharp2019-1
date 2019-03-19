f1fc91869ce4734e84c832d46af6c9ec6d543f60
kruno89 <kkukuc031@gmail.com>
2019-03-19 16:02:11 +0100

    prvi primjeri SQL

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿-- linijski komentar
+/* blok komentar
+CREATE DATABASE upisi
+GO
+*/
+USE upisi;
+create table #privremena(
+id int, 
+ime nchar(50),
+prezime varchar(50),
+mjesto_stanovanja nchar(25)
+);
+INSERT INTO [dbo].[#privremena] (
+[id], [ime], [prezime], [mjesto_stanovanja]) 
+VALUES (0, N'Pero                                              ', 
+N'Peric',
+N'Osijek                   ');
+select * from #privremena;
---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+use upisi;
+alter table polaznici
+add broj_telefona int
+go
+use upisi;
+alter table polaznici
+drop column broj_telefona
+go
